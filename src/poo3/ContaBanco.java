e36b4d5940e8f432f9a1d2e98c33c15aaf7ae79c
Matheus silva <teus_26@hotmail.com>
2021-04-29 15:12:31 -0300

    Aula 03

--- src/poo3/ContaBanco.java
+++ src/poo3/ContaBanco.java
@@ -7,13 +7,80 @@ public class ContaBanco {
     private float saldo;
     private boolean status;
     
+    public void estadoAtual(){
+        System.out.println("------------------------------");
+        System.out.println("Conta: " + this.numConta);
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.getStatus());
+    }
+    
+    
+    //MÉTODOS
+    public void abrirConta(String t) {
+        this.setTipo(t);
+        this.setStatus(true);
+        
+        if(t == "CC"){
+        this.setSaldo(50);
+        } else if(t == "CP"){
+        this.setSaldo(150);
+    }
+        System.out.println("Conta aberta com sucesso!");
+        
+    }
+    public void fecharConta() {
+        if(this.getSaldo() > 0){
+            System.out.println("Conta com dinheiro, conta não pode ser fechada.");
+        } else if(this.getSaldo() < 0){
+            System.out.println("Conta em débito.");
+        } else{
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso!");
+        }
+    }
+    public void depositar(float v) {
+        if(this.getStatus()){
+            this.setSaldo(this.getSaldo() + v);
+            System.out.println("Depósito realizado na conta de " + this.getDono());
+        } else {
+            System.out.println("Impossível depositar em uma conta fechada.");
+        }
+    }
+    public void sacar(float v) {
+        if(this.getStatus()){
+            if(this.getSaldo() >= v){
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saque realizado na conta de " + this.getDono());
+            } else {
+                System.out.println("Saldo insuficiente para saque");
+            }
+        } else {
+            System.out.println("Impossível sacar de uma conta fechada.");
+        }
+    }
+    public void pagarMensal() {
+        int v = 0;
+        if(this.getTipo() == "CC"){
+            v = 12;
+        } else if (this.getTipo() == "CP"){
+            v = 20;
+        }
+        if(this.getStatus()){
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mensalidade paga com sucesso por " + this.getDono());
+        } else {
+            System.out.println("Impossível pagar uma conta fechada.");
+        }
+    }
+    
     //CONSTRUTOR
-    public ContaBanco(float saldo, boolean status) {
+    ContaBanco() {
         this.saldo = 0;
         this.status = false;
     }
     
-    //GET E SET
     public void setNumConta(int n){
         this.numConta = n;
     }
@@ -48,63 +115,4 @@ public class ContaBanco {
     public boolean getStatus(){
         return status;
     }
-
-    //MÉTODOS
-    public void abrirConta(String t) {
-        setTipo(t);
-        setStatus(true);
-        
-        if(t == "CC"){
-        setSaldo(50);
-        } else if(t == "CP"){
-        setSaldo(150);
-    }
-        
-    }
-    public void fecharConta() {
-        if(saldo > 0){
-            System.out.println("Conta com dinheiro, conta não pode ser fechada.");
-        } else if(saldo < 0){
-            System.out.println("Conta em débito.");
-        } else{
-            setStatus(false);
-        }
-    }
-    public void depositar(float v) {
-        if(status == true){
-            saldo = saldo + v;
-        } else {
-            System.out.println("Impossível depositar");
-        }
-    }
-    public void sacar(float v) {
-        if(status == true){
-            if(saldo > v){
-                saldo = saldo - v;
-            } else {
-                System.out.println("Saldo insuficiente");
-            }
-        } else {
-            System.out.println("Impossível sacar");
-        }
-    }
-    public void pagarMensal() {
-        float v = 0;
-        if(tipo == "CC"){
-            v = 12;
-        } else if(tipo == "CP") {
-            v = 20;
-        }
-        
-        if(status == true){
-            if(saldo > v){
-                saldo = saldo - v;
-            } else {
-                System.out.println("Saldo insuficiente");
-            }
-        } else {
-            System.out.println("Impossível pagar");
-        }
-    }
-    
 }
